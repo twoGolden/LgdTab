f93ff719c9fd5dbbf43e702527dc270cfacd9fb3
zear <1403706282@qq.com>
2021-03-04 09:40:01 +0800

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# LgdTab
-tab
+# 组件说明
+## 参数说明
+| 参数名 | 说明 | 例子 | 是否必填 | 参数类型 |
+|:-----|---|-----|:-----:|:-----:|
+|windowsWidth|屏幕宽度|通过用getSystemInfo获取|是|NumBer|
+|tabValue|Tab数组|["放入所需显示文字","测试"]|是| Array |
+|textColor|文字/下划线颜色|"#34b2fa"|否|String|
+|fontSize|字体大小|16|否|NumBer|
+
+## 示例
+* 父页面获取当前tab下标
+
+> template
+
+	<lgd-tab :windowsWidth="windowsWidth" :tabValue="tabValue" @getIndex ="getIndex"/>
+
+> script
+
+	getIndex(index) { console.log("当前下标:" + index) }
+
+* 父页面更改当前tab下标
+
+> template
+
+	<lgd-tab :windowsWidth="windowsWidth" :tabValue="tabValue" ref="tabs"/>
+
+> script
+
+	this.$refs.tabs.clickTab(0)
+
+## 隐藏滚动条
+
+在App.vue样式中引入css，参考: <https://ask.dcloud.net.cn/article/36090>
+
+	<style>
+		@import url("./components/lgd-tab/lgd-tab.css");
+	</style>
+
+### 组件文档：<https://uniapp.dcloud.io/vue-components>
---
+++ components/lgd-tab/lgd-tab.vue
@@ -0,0 +1,132 @@
+<template>
+	<view>
+		<scroll-view class="scrollView" scroll-x show-scrollbar="false" :scroll-left="scrollLeft" scroll-with-animation>
+			<view class="tabBox" :style="{ 'justify-content': isOutWindow ? '' : 'space-around' }">
+				<view class="items" v-for="(item, index) in tabValue" :key="index" @click="clickTab(index)">
+					<text class="tabText" :class="index == tIndex ? 'active' : ''" :style="{ 'font-size': fontSize + 'px', color: index == tIndex ? textColor : ''}">
+						{{item}}
+					</text>
+				</view>
+			</view>
+			<view class="underscore" :style="{ width: inderWidth + 'px', 'margin-left': indexLeft + boxLeft + 'px', 'background-color': textColor, height: '3px' }" />
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			windowsWidth: { // 屏幕宽度
+				type: Number,
+				default: 0,
+				required: true
+			},
+			tabValue: { // tab数据
+				type: Array,
+				default: [],
+				required: true
+			},
+			textColor: { // 颜色
+				type: String,
+				default: '#34b2fa'
+			},
+			fontSize: { // 字体大小
+				type: Number,
+				default: 16
+			}
+		},
+		data() {
+			return {
+				boxLeft: 0,
+				isOutWindow: false,
+				inderWidth: 0,
+				indexLeft: 0,
+				scrollLeft: 0,
+				tIndex: 0,
+			};
+		},
+		methods: {
+			clickTab(index) {
+				// 更改选中下标
+				this.tIndex = index
+				this.$emit("getIndex", index)
+				// 获取盒子移动距离
+				if (this.isOutWindow && index >= 0) {
+					uni.createSelectorQuery().in(this).select(".tabBox").boundingClientRect().exec((data) => {
+						if (index == 0) {
+							this.boxLeft = 0
+						} else {
+							// 移动距离
+							this.boxLeft = -data[0].left
+						}
+					})
+				}
+				uni.createSelectorQuery().in(this).selectAll(".items").boundingClientRect().exec((data) => {
+					let width = data[0][index].width
+					let left = data[0][index].left
+					let newLeft = 0
+
+					// tab超出屏幕
+					if (this.isOutWindow) {
+						if (index == 0) {
+							left = 0
+						}
+
+						// 防止移动后距离缩短问题
+						for (let i = 0; i < index; i++) {
+							newLeft += data[0][i].width
+						}
+
+						// 是否需要居中
+						if (this.$props.windowsWidth / 2 < newLeft + width) {
+							this.scrollLeft = width / 2 - (this.$props.windowsWidth / 2 - newLeft)
+						} else {
+							this.scrollLeft = 0
+						}
+					}
+					// 点击tab宽度
+					this.inderWidth = width / 2
+					// 移动距离
+					this.indexLeft = (width - width / 2) / 2 + left
+				})
+			}
+		},
+		mounted() {
+			// 判断tab是否超出屏幕
+			uni.createSelectorQuery().in(this).selectAll(".items").boundingClientRect().exec((data) => {
+				if (data[0][this.tabValue.length - 1].right > this.$props.windowsWidth) {
+					this.isOutWindow = true
+				}
+			})
+			this.clickTab(0)
+		}
+	}
+</script>
+
+<style>
+	.scrollView {
+		width: 100%;
+		white-space: nowrap;
+	}
+
+	.tabBox {
+		display: flex;
+		align-items: center;
+	}
+
+	.items {
+		padding: 6px 20rpx;
+	}
+
+	.tabText {
+		color: #666666;
+	}
+
+	.active {
+		font-weight: bold;
+	}
+
+	.underscore {
+		transition: .2s all;
+	}
+</style>
