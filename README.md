fa56c694a446db2a164a0697146c0b5a217fa394
twoGolden <1403706282@qq.com>
2022-06-29 20:34:45 +0800

    修复BUG

--- README.md
+++ README.md
@@ -18,21 +18,21 @@
 | 参数名 | 说明 | 默认 | 是否必填 | 参数类型 |
 |:-----|---|-----|:-----:|:-----:|
 |tabValue|tab列表||是| Array |
-|tabIndex|tab选中下标|0|否| NumBer |
+|tabIndex|tab选中下标|0|否| Number |
 |textColor|文字颜色|#333|否|String|
 |underlineColor|滑块颜色|#34b2fa|否|String|
-|fontSize|字体大小|16|否|NumBer|
+|fontSize|字体大小|16|否|Number|
 |background|组件背景颜色|#fff|否|String|
 
 ## 事件
 | 事件名 | 说明 | 回调参数 |
 |:-----|-------|:-----:|
-|getIndex|获取当前点击下标值|tabIndex |
+|getIndex|获取当前点击下标值|tabIndex|
 
 ## 方法
 | 方法名 | 说明 | 传入参数 |
 |:-----|-------|:-----:|
-|clickTab|选中传入下标tab|tabIndex |
+|clickTab|选中传入的下标的tab|tabIndex|
 
 ## 隐藏滚动条
 
--- components/lgd-tab/lgd-tab.vue
+++ components/lgd-tab/lgd-tab.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+	let valueChange = false; // 值是否更改
 	export default {
 		props: {
 			tabValue: { // tab数据
@@ -58,12 +59,34 @@
 			};
 		},
 		watch: {
+			// 监听TAB内容更改
+			tabValue() {
+				valueChange = true
+				this.$nextTick(() => {
+					this.init()
+				})
+			},
 			// 监听传入下标更改
 			tabIndex(newValue, oldValue) {
-				this.clickTab(newValue)
+				if (!valueChange) this.clickTab(newValue)
 			}
 		},
 		methods: {
+			/// 初始化
+			init() {
+				let that = this;
+				// 判断tab是否超出屏幕
+				uni.createSelectorQuery().in(this).selectAll(".items").boundingClientRect().exec((data) => {
+					if (data[0][that.tabValue.length - 1].right > that.windowsWidth) {
+						that.isOutWindow = true
+					}
+					that.$nextTick(() => {
+						that.clickTab(that.tabIndex)
+					})
+				})
+			},
+			
+			/// 点击TAB
 			clickTab(index) {
 				// 更改选中下标
 				this.tIndex = index
@@ -83,18 +106,15 @@
 					let width = data[0][index].width
 					let left = data[0][index].left
 					let newLeft = 0
-
 					// tab超出屏幕
 					if (this.isOutWindow) {
 						if (index == 0) {
 							left = 0
 						}
-
 						// 防止移动后距离缩短问题
 						for (let i = 0; i < index; i++) {
 							newLeft += data[0][i].width
 						}
-
 						// 是否需要居中
 						if (this.windowsWidth / 2 < newLeft + width) {
 							this.scrollLeft = width / 2 - (this.windowsWidth / 2 - newLeft)
@@ -106,28 +126,16 @@
 					this.inderWidth = width / 2
 					// 移动距离
 					this.indexLeft = (width - width / 2) / 2 + left
+					// 重置
+					valueChange = false
 				})
 			}
 		},
-		created() {
-			let that = this;
-			// 获取屏幕宽度
-			uni.getSystemInfo({
-				success(res) {
-					that.windowsWidth = res.windowWidth
-				}
-			})
-		},
 		mounted() {
-			// 判断tab是否超出屏幕
-			uni.createSelectorQuery().in(this).selectAll(".items").boundingClientRect().exec((data) => {
-				if (data[0][this.tabValue.length - 1].right > this.windowsWidth) {
-					this.isOutWindow = true
-				}
-				this.$nextTick(() => {
-					this.clickTab(this.tabIndex)
-				})
-			})
+			// 获取屏幕宽度
+			this.windowsWidth = uni.getSystemInfoSync().windowWidth
+			// 初始化
+			this.init()
 		}
 	}
 </script>
